1e85b4dfc68b19c3b9e1a5d8c17b62b37eaf5461
Manjiri <manjiri1431@outlook.com>
2018-03-18 23:41:02 +0530

    First Code

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] ints = {2,5,2,8,5,4,6};
+        int k = 2;
+    }
+
+    public void process(int k, int[] ints){
+        List<List<Integer>> nodes = new ArrayList<>();
+        for (int i = 0 ; i < ints.length; i++){
+
+        }
+    }
+}
